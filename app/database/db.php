779ba2f92120ca5a5e38ef67d84dd02ddee202c4
whiskastheshiba <mirka8189@gmail.com>
2022-11-27 23:23:09 +0200

    Added new functions to the database.php

--- app/database/db.php
+++ app/database/db.php
@@ -8,21 +8,70 @@ function tt($value) {
     echo '</pre>';
 }
 
-function SelectAll($table) {
-    global $pdo;
-
-    $sql = "SELECT * FROM $table";
-    $query = $pdo->prepare($sql);
-    $query->execute();
+function dbCheckError($query){
     $errInfo = $query->errorInfo();
-
     if ($errInfo[0] !== PDO::ERR_NONE) {
         echo $errInfo[2];
         exit();
     }
+    return true;
+}
 
+function selectAll($table, $params = []) {
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+    if(!empty($params)) {
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'".$value."'";
+            }
+            if ($i === 0){
+                $sql =$sql . " WHERE $key = $value";
+            }
+            else {
+                $sql =$sql . " AND $key = $value";
+            }
+            $i++;
+        }
+    }
+    
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
     return $query->fetchAll();
 }
 
+function selectOne($table, $params = []) {
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+    if(!empty($params)) {
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'".$value."'";
+            }
+            if ($i === 0){
+                $sql =$sql . " WHERE $key = $value";
+            }
+            else {
+                $sql =$sql . " AND $key = $value";
+            }
+            $i++;
+        }
+    }
+    //$sql = $sql . " LIMIT 1";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+    return $query->fetch();
+}
 
-tt(selectAll('test'));
+$params = [
+    'admin' => 1,
+    //'email' => 'tester@test.com'
+];
+
+
+//tt(selectAll('test', $params));
+tt(selectOne('test', $params));
